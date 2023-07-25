a37573c714c6aa00a535e3aae6e9152cd265d95b
root <povtandseo@gmail.com>
2023-07-26 01:43:37 +0400

    Add guards in controllers/user.rb

--- controllers/user.rb
+++ controllers/user.rb
@@ -12,32 +12,29 @@ class UserController
 
   def create_user
     parsed_body = JSON.parse(@raw_body)
-    errors = []
-    errors << "Fiend 'name' is absent. \n" unless parsed_body["name"]
-    errors << "Field 'age' is absent. \n" unless parsed_body["age"]
 
-    if errors.any?
-      [422, {}, errors]
-    else 
-      new_user = User.new(parsed_body["name"], parsed_body["age"].to_i)
-      if new_user.valid
-        redis = Redis.new
-        redis.set(parsed_body["name"], @raw_body)
-        [201, {}, ["User has been created"]]
-      else
-        [422, {}, new_user.errors]
-      end
-    end
+    errors = []
+    errors << "Field 'name' is absent. \n" unless parsed_body["name"]
+    errors << "Field 'age' is absent. \n" unless parsed_body["age"]
+    return [422, {}, errors] if errors.any?
+
+    new_user = User.new(parsed_body["name"], parsed_body["age"].to_i)
+    return [422, {}, new_user.errors] unless new_user.valid
+
+    redis = Redis.new
+    redis.set(parsed_body["name"], @raw_body)
+    
+    [201, {}, ["User has been created"]]
   end
+
 
   def find_user
     requested_name = @parsed_request.path.gsub('/api/user/',"")
     redis1 = Redis.new
     output = redis1.get(requested_name)
-    if output
-      [200, {}, [output]] 
-    else
-      [404, {}, ["User #{requested_name} is not found"]]
-    end
-  end 
+
+    return [404, {}, ["User #{requested_name} is not found"]] unless output
+
+    [200, {}, [output]] 
+  end
 end
